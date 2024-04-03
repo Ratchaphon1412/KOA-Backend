cd167558935fa05f1bb0e3255cb3ebcadd02a4a8
Ratchaphon1412 <Ratchaphon.h111@gmail.com>
2024-04-03 16:02:56 +0700

    create comment schema that relation with todo schema

--- api/todo/todo.model.js
+++ api/todo/todo.model.js
@@ -23,10 +23,36 @@ const TodoSchema = new Schema({
         type: Date,
         default: Date.now,
     },
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+    }],
+
 });
 
-const Todo = mongoose.model('Todo', TodoSchema);
+
+const CommentSchema = new Schema({
+    todo_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Todo',
+        required: true,
+    },
+
+    content: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
 
-export default Todo;
+
+export const Todo = mongoose.model('Todo', TodoSchema);
+export const Comment = mongoose.model('Comment', CommentSchema);
+
+
+
  
